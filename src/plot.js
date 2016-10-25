bf81c28c046dd7878f7ffc5e017bd093ae07de6c
jberlin <n0tan3rd@gmail.com>
2016-10-25 18:11:29 -0400

    semi working

--- src/plot.js
+++ src/plot.js
@@ -1,72 +1,99 @@
 import moment from  'moment'
-import * as d3 from 'd3'
-import mg from 'metrics-graphics'
-// const d3 = require('d3')
-// const nv = require('nvd3')
+import axios from 'axios'
+import vis from 'vis'
 
-console.log('test')
-var chart
-d3.json('plot/Accept.json', data => {
-  console.log(data)
-  let mp = []
-  let members = new Set()
-  data.member_points.forEach(m => {
-    // members.add(m.member)
-    // console.log(moment(m.f,'YYYYMMDDHHMMSS').year(),moment(m.t,'YYYYMMDDHHMMSS').year())
 
-    mp.push({
-      date: moment(m.t,'YYYYMMDDHHMMSS').toDate(),
-      value: m.member
+
+axios.get('plot/Annihilator.json')
+  .then(data => {
+    let ds = new vis.DataSet({})
+    console.log(data)
+    let md = data.data
+    let mp = []
+    let c = 0
+    md.member_points.forEach(m => {
+      // members.add(m.member)
+      console.log(m.member,moment(m.t,'YYYYMDDHHmmss').format(),moment(m.f,'YYYYMDDHHmmss').format())
+      mp.push({
+        start: moment(m.t,'YYYYMDDHHmmss'),
+        end: moment(m.f,'YYYYMDDHHmmss'),
+        content: m.member
+      })
+
     })
-
-    mp.push({
-      date: moment(m.f,'YYYYMMDDHHMMSS').toDate(),
-      value: m.member
-    })
-
-
+    console.log(mp)
+    ds.add(mp)
+    //
+    const plotElem = new vis.Timeline(document.getElementById('chart'), ds, {})
   })
-
-  // let y = d3.scaleOrdinal()
-  //   .range([0, window.innerHeight])
-  //   .domain(Array.from(members.values()))
-  //
-  // //2016 10 25 05 10 84
-  //
-  // mp = mg.convert.date(mp,'date','%Y-%m-%d')
-  // console.log(mp)
-  //
-  mg.data_graphic({
-    title: "Accept",
-    data: mp,
-    left: 200,
-    full_width: true,
-    height: 500,
-    chart_type: 'bar',
-    missing_is_hidden: true,
-    y_scale_type: 'ordinal',
-    x_scale_type: 'time',
-    target: '#chart',
-    x_accessor: 'date',
-    y_accessor: 'value',
+  .catch(err => {
+    console.log(err)
   })
-  // let md = {
-  //   "key": "mementos",
-  //   "values": []
-  // }
-  // let plotd = []
-  // data.mementos.forEach(m => {
-  //   console.log(m)
-  //   md.values.push({
-  //     label: 'memento',
-  //     value: m.datetime
-  //   })
-  // })
-  // console.log()
-  // nv.addGraph(() => {
-  //   // chart = nv.models.multiBarHorizontalChart()
-  //   //   .x(d => d.label)
-  //   //   .y(d => d.value)
-  //   //   .
-  // })
-})
+// import * as d3 from 'd3'
+// import mg from 'metrics-graphics'
+// // const d3 = require('d3')
+// // const nv = require('nvd3')
+//
+// console.log('test')
+// var chart
+// d3.json('plot/Accept.json', data => {
+//   console.log(data)
+//   let mp = []
+//   let members = new Set()
+//   data.member_points.forEach(m => {
+//     // members.add(m.member)
+//     // console.log(moment(m.f,'YYYYMMDDHHMMSS').year(),moment(m.t,'YYYYMMDDHHMMSS').year())
+//
+//     mp.push({
+//       date: moment(m.t,'YYYYMMDDHHMMSS').toDate(),
+//       value: m.member
+//     })
+//
+//     mp.push({
+//       date: moment(m.f,'YYYYMMDDHHMMSS').toDate(),
+//       value: m.member
+//     })
+//
+//
+//   })
+//
+//   // let y = d3.scaleOrdinal()
+//   //   .range([0, window.innerHeight])
+//   //   .domain(Array.from(members.values()))
+//   //
+//   // //2016 10 25 05 10 84
+//   //
+//   // mp = mg.convert.date(mp,'date','%Y-%m-%d')
+//   // console.log(mp)
+//   //
+//   mg.data_graphic({
+//     title: "Accept",
+//     data: mp,
+//     left: 200,
+//     full_width: true,
+//     height: 500,
+//     missing_is_hidden: true,
+//     target: '#chart',
+//     x_accessor: 'date',
+//     y_accessor: 'value',
+//   })
+//   // let md = {
+//   //   "key": "mementos",
+//   //   "values": []
+//   // }
+//   // let plotd = []
+//   // data.mementos.forEach(m => {
+//   //   console.log(m)
+//   //   md.values.push({
+//   //     label: 'memento',
+//   //     value: m.datetime
+//   //   })
+//   // })
+//   // console.log()
+//   // nv.addGraph(() => {
+//   //   // chart = nv.models.multiBarHorizontalChart()
+//   //   //   .x(d => d.label)
+//   //   //   .y(d => d.value)
+//   //   //   .
+//   // })
+// })
